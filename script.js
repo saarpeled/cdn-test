58c9f5d062908412e163a2539bf0365150f101d3
ben.iisr.2004@gmail.com <ben.iisr.2004@gmail.com>
2024-03-10 15:41:36 +0200

    secure code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 // Malicous code to be injected into the website
-document.body.innerHTML += `<img src="https://i.postimg.cc/qq491mdJ/threat-actor.webp" alt="hacked" width="1000" height="1000">`;
-alert("You have been hacked!");
+// document.body.innerHTML += `<img src="https://i.postimg.cc/qq491mdJ/threat-actor.webp" alt="hacked" width="1000" height="1000">`;
+// alert("You have been hacked!");
 
 
 // Secure code example
-// document.body.innerHTML += `<h1> This is a secure website </h1>`;
+document.body.innerHTML += `<h1> This is a secure website </h1>`;
 
